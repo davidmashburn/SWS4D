b56cdbde11c5008ade2c31aa46fc82b0cef5724b
David N. Mashburn <david.n.mashburn@gmail.com>
2012-05-01 15:27:32 -0500

    Old 5

--- mlabArrayViewer.py
+++ mlabArrayViewer.py
@@ -9,36 +9,6 @@ from mayavi.core.ui.api import MayaviScene, SceneEditor, MlabSceneModel
 from mayavi import mlab
 from tvtk.api import tvtk
 
-class ArrayView4D(HasTraits):
-    low = Int(0)
-    tlength = Property(depends_on=['arr'])
-    zlength = Property(depends_on=['arr'])
-
-    def _get_tlength(self):
-        return self.arr.shape[0]-1
-    def _get_zlength(self):
-        return self.arr.shape[1]-1
-
-    tindex = Range(low='low', high='tlength', value=0, exclude_high=False, mode='spinner')
-    zindex = Range(low='low', high='zlength', value=0, exclude_high=False, mode='spinner')
-    
-    arr = Array(shape=[None]*4)
-    
-    scene = Instance(MlabSceneModel, ())
-    plot = Instance(PipelineBase)
-    
-    @on_trait_change('zindex,tindex')
-    def update_plot(self):
-        print self.zindex, self.tindex
-        if self.plot is None:
-            self.plot = self.scene.mlab.imshow( self.arr[self.tindex,self.zindex], colormap='gray' )
-        else:
-            self.plot.mlab_source.set(scalars=self.arr[self.tindex,self.zindex])
-    
-    # The layout of the dialog created
-    view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
-                Group('zindex', 'tindex'), resizable=True)
-
 class ArrayViewTriIPW(HasTraits):
     low = Int(0)
     tlength = Property(depends_on=['arr'])
@@ -172,122 +142,6 @@ class ArrayViewTriIPW(HasTraits):
     view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
                 Group('xindex','yindex','zindex', 'tindex'), resizable=True)
 
-class ArrayView4DTriBlob(HasTraits):
-    low = Int(0)
-    tlength = Property(depends_on=['arr'])
-    zlength = Property(depends_on=['arr'])
-    ylength = Property(depends_on=['arr'])
-    xlength = Property(depends_on=['arr'])
-
-    def _get_tlength(self):
-        return self.arr.shape[0]-1
-    def _get_zlength(self):
-        return self.arr.shape[1]-1
-    def _get_ylength(self):
-        return self.arr.shape[2]-1
-    def _get_xlength(self):
-        return self.arr.shape[3]-1
-
-    tindex = Range(low='low', high='tlength', value=1, exclude_high=False, mode='slider') # or spinner
-    zindex = Range(low='low', high='zlength', value=1, exclude_high=False, mode='slider')
-    yindex = Range(low='low', high='ylength', value=1, exclude_high=False, mode='slider')
-    xindex = Range(low='low', high='xlength', value=1, exclude_high=False, mode='slider')
-    flip = Bool(False)
-    
-    arr = Array(shape=[None]*4)
-    
-    scene = Instance(MlabSceneModel, ())
-    plot = Instance(PipelineBase)
-    
-    @on_trait_change('xindex,yindex,zindex,tindex')
-    def update_plot(self):
-        arr = self.arr
-        print self.xindex, self.yindex,self.zindex, self.tindex
-        if not hasattr(self,'min'):
-            self.min = arr.min()
-            self.max = arr.max()
-        
-        buffer = 10
-        arrExp = np.zeros([arr.shape[2]+arr.shape[1]+buffer,arr.shape[3]+arr.shape[1]+buffer],dtype=arr.dtype)
-        xt = arr[self.tindex,:,:,self.xindex].T
-        yt = arr[self.tindex,:,self.yindex]
-        zt = arr[self.tindex,self.zindex]
-        arr_min = min(xt.min(),yt.min,zt.min())
-        arr_max = max(xt.max(),yt.max(),zt.max())
-
-        arrExp[-arr.shape[2]:,-arr.shape[3]:] = zt
-        arrExp[-arr.shape[2]:,:arr.shape[1]] = xt
-        arrExp[:arr.shape[1],-arr.shape[3]:] = yt
-        #if self.flip:
-        #    arrExp = arrExp.T
-        # might be better to set the view here instead...
-        
-        if self.plot is None:
-            self.plot = self.scene.mlab.imshow( arrExp, colormap='gray', vmin=arr.min(), vmax=arr.max())
-        else:
-            self.plot.mlab_source.set(scalars=arrExp)
-    
-    # The layout of the dialog created
-    view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
-                Group('xindex','yindex','zindex', 'tindex'), resizable=True)
-
-class ArrayView4DTri(HasTraits):
-    low = Int(0)
-    tlength = Property(depends_on=['arr'])
-    zlength = Property(depends_on=['arr'])
-    ylength = Property(depends_on=['arr'])
-    xlength = Property(depends_on=['arr'])
-
-    def _get_tlength(self):
-        return self.arr.shape[0]-1
-    def _get_zlength(self):
-        return self.arr.shape[1]-1
-    def _get_ylength(self):
-        return self.arr.shape[2]-1
-    def _get_xlength(self):
-        return self.arr.shape[3]-1
-
-    tindex = Range(low='low', high='tlength', value=1, exclude_high=False, mode='slider')
-    zindex = Range(low='low', high='zlength', value=1, exclude_high=False, mode='spinner')
-    yindex = Range(low='low', high='ylength', value=1, exclude_high=False, mode='spinner')
-    xindex = Range(low='low', high='xlength', value=1, exclude_high=False, mode='spinner')
-    
-    arr = Array(shape=[None]*4)
-    
-    sceneXY = Instance(MlabSceneModel, ())
-    sceneXZ = Instance(MlabSceneModel, ())
-    sceneYZ = Instance(MlabSceneModel, ())
-    
-    plotXY = Instance(PipelineBase)
-    plotXZ = Instance(PipelineBase)
-    plotYZ = Instance(PipelineBase)
-    
-    @on_trait_change('tindex,zindex,yindex,xindex')
-    def update_plot(self):
-        if self.plotXY is None:
-            self.plotXY = mlab.imshow(self.arr[self.tindex,self.zindex,:,:], colormap='gray', figure=self.sceneXY.mayavi_scene)  
-            self.plotXZ = mlab.imshow(self.arr[self.tindex,:,self.yindex,:], colormap='gray', figure=self.sceneXZ.mayavi_scene)
-            self.plotYZ = mlab.imshow(self.arr[self.tindex,:,:,self.xindex].T, colormap='gray', figure=self.sceneYZ.mayavi_scene)
-        else:
-            self.plotXY.mlab_source.set(scalars=self.arr[self.tindex,self.zindex,:,:])
-            self.plotXZ.mlab_source.set(scalars=self.arr[self.tindex,:,self.yindex,:])
-            self.plotYZ.mlab_source.set(scalars=self.arr[self.tindex,:,:,self.xindex].T)
-
-    
-    # The layout of the dialog created
-    view = View(
-                VGroup(
-                  VSplit(
-                    Item('sceneYZ', editor=SceneEditor(scene_class=MayaviScene), height=300, width=300, show_label=False),
-                    HSplit(
-                        Item('sceneXY', editor=SceneEditor(scene_class=MayaviScene), height=300, width=300, show_label=False),
-                        Item('sceneXZ', editor=SceneEditor(scene_class=MayaviScene), height=300, width=300, show_label=False),
-                    ),
-                  ),
-                  Group('xindex','yindex','zindex', 'tindex'),
-                ),
-                resizable=True)
-
 if __name__=='__main__':
     arr = np.array([ [[[1,2,3,4],[5,6,7,8],[9,10,11,12]],[[1,2,3,4],[5,6,7,8],[9,10,11,12]]], [[[1,2,3,4],[5,6,7,8],[9,10,11,12]],[[1,2,3,4],[5,6,7,8],[9,10,11,12]]] ])
     arr[1] = np.sqrt(arr[1])
@@ -296,9 +150,9 @@ if __name__=='__main__':
     import testArrays
     arr = testArrays.abb3D
     import GifTiffLoader as GTL
-    numLoad=1
-    name = '/media/home/ViibreData/ActiveData/NewSegmentation/AS Edge Wound Healing/2009SEPT24CellCycle05_W2X/','','2009SEPT24Cell_Cycle05_2X20s.tif'
-    #name = '/home/mashbudn/Documents/VIIBRE--ScarHealing/ActiveData/Resille/2012-04-11/1/Riselle_t','1','.TIF'
+    numLoad=15
+    #name = '/media/home/ViibreData/ActiveData/NewSegmentation/AS Edge Wound Healing/2009SEPT24CellCycle05_W2X/','','2009SEPT24Cell_Cycle05_2X20s.tif'
+    name = '/home/mashbudn/Documents/VIIBRE--ScarHealing/ActiveData/Resille/2012-04-11/1/Riselle_t','1','.TIF'
     arr0 = GTL.LoadMonolithic(''.join(name))
     arr = np.zeros([numLoad]+list(arr0.shape),dtype=arr0.dtype)
     arr[0] = arr0
