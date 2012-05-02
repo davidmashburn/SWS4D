23348eb985dc7053878417eb7418e5085ff36382
David N. Mashburn <david.n.mashburn@gmail.com>
2012-05-02 09:45:48 -0500

    Old 11 - 10.5 plus convert plots to dict for cleanup

--- mlabArrayViewer.py
+++ mlabArrayViewer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 
-from traits.api import HasTraits, Int, Range, Bool, Instance, Property, Array, List, Button, on_trait_change
+from traits.api import HasTraits, Int, Range, Bool, Instance, Property, Array, List, Dict, Button, on_trait_change
 from traitsui.api import View, Item, Group, RangeEditor, VGroup, HGroup, VSplit, HSplit, NullEditor
 from mayavi.core.api import PipelineBase
 from mayavi.core.ui.api import MayaviScene, SceneEditor, MlabSceneModel
@@ -37,13 +37,11 @@ class ArrayView4D(HasTraits):
     scene = Instance(MlabSceneModel, ())
     scene2 = Instance(MlabSceneModel, ())
     
-    plotXY = Instance(PipelineBase)
-    plotXZ = Instance(PipelineBase)
-    plotYZ = Instance(PipelineBase)
+    plots = Dict()
+    plots2 = Dict()
     
-    plotXY2 = Instance(PipelineBase)
-    plotXZ2 = Instance(PipelineBase)
-    plotYZ2 = Instance(PipelineBase)
+    cursors = Dict()
+    cursors2 = Dict()
     
     # The layout of the dialog created
     view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
@@ -58,10 +56,7 @@ class ArrayView4D(HasTraits):
         self.cursors = {'x':None, 'y':None, 'zx':None, 'zy':None}        
         self.cursorSize = cursorSize
     
-    def display_scene_helper(self,arr,scene,cursors,number=''): # Number is either nothing or '2'
-        if number not in ['','2']:
-            print "Number must be '' or '2'"
-            return
+    def display_scene_helper(self,arr,scene,cursors,plots): # Number is either nothing or '2'
         # Interaction properties can only be changed after the scene
         # has been created, and thus the interactor exists
         #self.scene.scene.background = (0, 0, 0)
@@ -92,14 +87,14 @@ class ArrayView4D(HasTraits):
         sYZ = scene.mlab.pipeline.array2d_source(x,y,xt)
         
         sList = [sXY,sXZ,sYZ]
-        pList = [i+number for i in ['plotXY','plotXZ','plotYZ']]
+        pList = ['XY','XZ','YZ']
         pFunc = scene.mlab.pipeline.image_plane_widget #scene.mlab.pipeline.image_actor
         for i in range(3):
-            #setattr( self, pList[i], pFunc(sList[i],interpolate=False,
-            #                               colormap='gray',vmin=arrMin,vmax=arrMax) )
-            setattr( self, pList[i], pFunc(sList[i],plane_orientation='z_axes',
-                                           colormap='gray',vmin=arrMin,vmax=arrMax) )
-            getattr( self, pList[i]).ipw.left_button_action = 0
+            #plots(pList[i]) = pFunc(sList[i],interpolate=False,
+            #                        colormap='gray',vmin=arrMin,vmax=arrMax) )
+            plots[pList[i]] = pFunc(sList[i],plane_orientation='z_axes',
+                                    colormap='gray',vmin=arrMin,vmax=arrMax)
+            plots[pList[i]].ipw.left_button_action = 0
             
         
         def quickLine(x,y): # pass one list and one value
@@ -124,48 +119,48 @@ class ArrayView4D(HasTraits):
     @on_trait_change('scene.activated')
     def display_scene(self):
         print 'Scene activated!'
-        self.display_scene_helper(self.arr,self.scene,self.cursors)
+        self.display_scene_helper(self.arr,self.scene,self.cursors,self.plots)
     @on_trait_change('scene2.activated')
     def display_scene2(self):
-        self.display_scene_helper(self.arr2,self.scene2,self.cursors2,number='2')
+        self.display_scene_helper(self.arr2,self.scene2,self.cursors2,self.plots2)
     @on_trait_change('tindex')
     def update_all_plots(self):
-        if self.plotXY is not None:
-            self.plotXY.mlab_source.scalars = self.arr[self.tindex,self.zindex]
-            self.plotXZ.mlab_source.scalars = self.arr[self.tindex,:,self.yindex,:]
-            self.plotYZ.mlab_source.scalars = self.arr[self.tindex,:,:,self.xindex].T
-        if self.plotXY2 is not None:
-            self.plotXY2.mlab_source.scalars = self.arr2[self.tindex,self.zindex]
-            self.plotXZ2.mlab_source.scalars = self.arr2[self.tindex,:,self.yindex,:]
-            self.plotYZ2.mlab_source.scalars = self.arr2[self.tindex,:,:,self.xindex].T
+        for i in range(2):
+            arr = [self.arr,self.arr2][i]
+            plots = [self.plots,self.plots2][i]
+            if plots is not {}:
+                plots['XY'].mlab_source.scalars = arr[self.tindex,self.zindex]
+                plots['XZ'].mlab_source.scalars = arr[self.tindex,:,self.yindex,:]
+                plots['YZ'].mlab_source.scalars = arr[self.tindex,:,:,self.xindex].T
     @on_trait_change('xindex')
     def update_x_plots(self):
-        if self.plotXY is not None:
-            self.plotYZ.mlab_source.scalars = self.arr[self.tindex,:,:,self.xindex].T
-            self.cursors['x'].mlab_source.set( y=[self.xindex]*2 )
-        if self.plotXY2 is not None:
-            self.plotYZ2.mlab_source.scalars = self.arr2[self.tindex,:,:,self.xindex].T
-            self.cursors2['x'].mlab_source.set( y=[self.xindex]*2 )
+        for i in range(2):
+            arr = [self.arr,self.arr2][i]
+            plots = [self.plots,self.plots2][i]
+            cursors = [self.cursors,self.cursors2][i]
+            if plots is not {}:
+                plots['YZ'].mlab_source.scalars = arr[self.tindex,:,:,self.xindex].T
+                cursors['x'].mlab_source.set( y=[self.xindex]*2 )
     @on_trait_change('yindex')
     def update_y_plots(self):
-        if self.plotXY is not None:
-            self.plotXZ.mlab_source.scalars = self.arr[self.tindex,:,self.yindex,:]
-            self.cursors['y'].mlab_source.set( x=[self.yindex]*2 )
-        if self.plotXY2 is not None:
-            self.plotXZ2.mlab_source.scalars = self.arr2[self.tindex,:,self.yindex,:]
-            self.cursors2['y'].mlab_source.set( x=[self.yindex]*2 )
+        for i in range(2):
+            arr = [self.arr,self.arr2][i]
+            plots = [self.plots,self.plots2][i]
+            cursors = [self.cursors,self.cursors2][i]
+            if plots is not {}:
+                plots['XZ'].mlab_source.scalars = arr[self.tindex,:,self.yindex,:]
+                cursors['y'].mlab_source.set( x=[self.yindex]*2 )
     @on_trait_change('zindex')
     def update_z_plots(self):
         xs,ys,zs = self.arr.shape[3],self.arr.shape[2],self.arr.shape[1]
-        
-        if self.plotXY is not None:
-            self.plotXY.mlab_source.scalars = self.arr[self.tindex,self.zindex]
-            self.cursors['zx'].mlab_source.set( x=[(self.zindex-zs)*self.zscale - self.plotBuffer]*2 )
-            self.cursors['zy'].mlab_source.set( y=[self.plotBuffer+xs+self.zindex*self.zscale]*2 )
-        if self.plotXY2 is not None:
-            self.plotXY2.mlab_source.scalars = self.arr2[self.tindex,self.zindex]
-            self.cursors2['zx'].mlab_source.set( x=[(self.zindex-zs)*self.zscale - self.plotBuffer]*2 )
-            self.cursors2['zy'].mlab_source.set( y=[self.plotBuffer+xs+self.zindex*self.zscale]*2 )
+        for i in range(2):
+            arr = [self.arr,self.arr2][i]
+            plots = [self.plots,self.plots2][i]
+            cursors = [self.cursors,self.cursors2][i]
+            if plots is not {}:
+                plots['XY'].mlab_source.scalars = arr[self.tindex,self.zindex]
+                cursors['zx'].mlab_source.set( x=[(self.zindex-zs)*self.zscale - self.plotBuffer]*2 )
+                cursors['zy'].mlab_source.set( y=[self.plotBuffer+xs+self.zindex*self.zscale]*2 )
 
 class ArrayView4DDual(ArrayView4D):
     # The layout of the dialog created
@@ -175,7 +170,38 @@ class ArrayView4DDual(ArrayView4D):
                 ),
                 Group('xindex','yindex','zindex', 'tindex')), resizable=True)
     def __init__(self,arr,arr2,cursorSize=2,**traits):
-        ArrayView4D.__init__(self,arr=arr,arr2=arr2,**traits) # Call __init__ on the super
+        ArrayView4D.__init__(self,arr=arr,arr2=arr2,cursorSize=cursorSize,**traits) # Call __init__ on the super
+
+class ArrayViewVolume(HasTraits):
+    low = Int(0)
+    tlength = Property(depends_on=['arr'])
+    def _get_tlength(self):
+        return self.arr.shape[0]-1
+    tindex = Range(low='low', high='tlength', value=0, exclude_high=False, mode='slider') # or spinner
+    
+    arr = Array(shape=[None]*4)
+    
+    scene = Instance(MlabSceneModel, ())
+    
+    vPlot = Instance(PipelineBase)
+    
+    # The layout of the dialog created
+    view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene), height=250, width=300, show_label=False),
+                Group('tindex'), resizable=True)
+    
+    def __init__(self,arr,vmin=None,vmax=None,**traits):
+        HasTraits.__init__(self,arr=arr,**traits) # Call __init__ on the super
+        self.vmin = (arr.min() if vmin==None else vmin)
+        self.vmax = (arr.max() if vmax==None else vmax)
+    
+    @on_trait_change('scene.activated')
+    def make_plot(self):
+        self.vPlot = self.scene.mlab.pipeline.volume(mlab.pipeline.scalar_field(self.arr[self.tindex]), vmin=self.vmin, vmax=self.vmax)
+    
+    @on_trait_change('tindex')
+    def update_plot(self):
+        self.vPlot.mlab_source.scalars = self.arr[self.tindex]
+
 
 if __name__=='__main__':
     arr = np.array([ [[[1,2,3,4],[5,6,7,8],[9,10,11,12]],[[1,2,3,4],[5,6,7,8],[9,10,11,12]]], [[[1,2,3,4],[5,6,7,8],[9,10,11,12]],[[1,2,3,4],[5,6,7,8],[9,10,11,12]]] ])
