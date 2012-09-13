993cb158e10d752ea73887aee791d1819e217d99
David N. Mashburn <david.n.mashburn@gmail.com>
2012-09-13 17:23:45 -0500

    Added CopySeedRegionsToAllFrames to SWS4D_utils.py

--- SWS4D/SWS4D_utils.py
+++ SWS4D/SWS4D_utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 '''Utility functions for SWS4D'''
+import numpy as np
+import coo_utils
 
 def ScrubCellID(sws4D,sh,cellID):
     '''Super-slow value clear...'''
@@ -10,3 +12,13 @@ def ScrubCellID(sws4D,sh,cellID):
                     if sws4d.seedLil[i][j][k,l] == cellID:
                         sws4d.seedLil[i][j][k,l] = 0
 
+def CopySeedRegionsToAllFrames(sws4d,valsToCopy,t=0,z=0):
+    '''This is a function to map one or more seed region (by id) in one frame (default 0,0) to all other frames. Most useful in defining a background region.'''
+    regions = [ (coo_utils.CooHDToArray(sws4d.seedLil[t][z])==i) for i in valsToCopy]
+    whs = [np.where(i) for i in regions]
+    for i in range(sws4d.arr.shape[0]):
+        for j in range(sws4d.arr.shape[1]):
+            a = coo_utils.CooHDToArray(sws4d.seedLil[i][j])
+            for k,v in enumerate(ValsToCopy):
+                a[whs[k]] = v
+            sws4d.seedLil[i][j] = coo_utils.ArrayToCooHD(a)
